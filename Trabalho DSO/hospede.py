f5f81c64034d719bdec5f84bd234263fed90ff47
Camila Martello <mvrtello.cvmila@gmail.com>
2025-09-13 23:17:37 -0300

    Realizei algumas alterções nas classes: Pessoa, Hospede (inclui os atributos) e Funcionario e add as classes que faltavam.

--- Trabalho DSO/hospede.py
+++ Trabalho DSO/hospede.py
@@ -1,10 +1,27 @@
+
 from pessoa import Pessoa
-from pessoa import cpf
-from pessoa import nome
-from pessoa import idade
-from pessoa import telefone
-from pessoa import email
 
 class Hospede(Pessoa):
-    def __init__(self, ):
+    def __init__(self, cpf:str, nome:str, idade:int, telefone:str, email:str, hosp_adulto:str, hosp_crianca:str ):
         super().__init__(cpf, nome, idade, telefone, email)
+        self.__hosp_adulto = hosp_adulto
+        self.__hosp_crianca = hosp_crianca
+    
+    @property
+    def hosp_adulto(self):
+        return self.__hosp_adulto
+    
+    @hosp_adulto.setter
+    def hosp_adulto(self, hosp_adulto):
+        self.__hosp_adulto = hosp_adulto
+
+    @property
+    def hosp_crianca(self):
+        return self.__hosp_crianca
+    
+    @hosp_crianca.setter
+    def hosp_crianca(self, hosp_crianca):
+        self.__hosp_crianca = hosp_crianca
+
+    def verifica_hospede(self):
+        pass
--- Trabalho DSO/hotel.py
+++ Trabalho DSO/hotel.py
@@ -1,2 +1,28 @@
+from hospede import Hospede
+from funcionario import Funcionario
+from quarto import Quarto
+from reserva import Reserva
+from recursos_humanos import Rh
+
 class Hotel:
-    def __init__(self,):
+    def __init__(self, nome:str, hospedes:Hospede, funcionarios:Funcionario, quartos:Quarto, reservas:Reserva, recursos_humanos:Rh):
+        if not isinstance(nome, str):
+            self.__nome = str(nome)
+        else:
+            self.__nome = nome
+        self.__hospedes = hospedes
+        self.__funcionarios = Funcionario[]
+        self.__quartos = []
+        self.__reservas = []
+        self.__recursos_humanos = recursos_humanos
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, nome:str):
+        self.__nome = nome
+
+    @property
+    def recursos_humanos(self):
+        return self.__recusros_humanos
