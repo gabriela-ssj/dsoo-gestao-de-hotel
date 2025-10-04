407e4ac5256df011de4c45a6049651fd2906113f
gabriela-ssj <gabrelajesus755@gmail.com>
2025-10-03 22:05:47 -0300

    implementação controlador e tela da classe rh

--- Trabalho DSO/controlers/controlador_recursos_humanos.py
+++ Trabalho DSO/controlers/controlador_recursos_humanos.py
@@ -1,2 +1,61 @@
+from entidades.recursos_humanos import Rh
+from entidades.funcionario import Funcionario
+from entidades.cargo import Cargo
+from telas.tela_recursos_humanos import TelaRh
 
-class ControladorRecursosHumanos():
+class ControladorRh:
+    def __init__(self, rh: Rh, controlador_sistema):
+        self.__rh = rh
+        self.__tela = TelaRh()
+        self.__controlador_sistema = controlador_sistema
+
+    def incluir_funcionario(self):
+        dados = self.__tela.pega_dados_funcionario()
+        cargo = Cargo(dados["tipo_cargo"])
+        funcionario = Funcionario(
+            cpf=dados["cpf"],
+            nome=dados["nome"],
+            idade=dados["idade"],
+            telefone=dados["telefone"],
+            email=dados["email"],
+            cargo=cargo
+        )
+        self.__rh.adicionar_funcionario(funcionario)
+
+    def alterar_funcionario(self):
+        cpf = self.__tela.seleciona_funcionario()
+        novos_dados = self.__tela.pega_dados_funcionario()
+        self.__rh.alterar_funcionario(cpf, novos_dados)
+
+    def excluir_funcionario(self):
+        cpf = self.__tela.seleciona_funcionario()
+        self.__rh.excluir_funcionario(cpf)
+
+    def listar_funcionarios(self):
+        lista = self.__rh.listar_funcionarios()
+        self.__tela.mostra_lista(lista)
+
+    def realizar_pagamento(self):
+        cpf = self.__tela.seleciona_funcionario()
+        metodo = self.__tela.pega_metodo_pagamento()
+        resultado = self.__rh.realizar_pagamento(cpf, metodo)
+        self.__tela.mostra_mensagem(resultado)
+
+    def retornar(self):
+        self.__controlador_sistema.abre_tela()
+
+    def abre_tela(self):
+        opcoes = {
+            1: self.incluir_funcionario,
+            2: self.alterar_funcionario,
+            3: self.excluir_funcionario,
+            4: self.listar_funcionarios,
+            5: self.realizar_pagamento,
+            0: self.retornar
+        }
+        while True:
+            opcao = self.__tela.tela_opcoes()
+            if opcao in opcoes:
+                opcoes[opcao]()
+            else:
+                self.__tela.mostra_mensagem("⚠️ Opção inválida.")
--- Trabalho DSO/telas/tela_recursos_humanos.py
+++ Trabalho DSO/telas/tela_recursos_humanos.py
@@ -1,2 +1,41 @@
+class TelaRh:
+    def tela_opcoes(self):
+        print("\n-------- MENU RH ----------")
+        print("1 - Incluir Funcionário")
+        print("2 - Alterar Funcionário")
+        print("3 - Excluir Funcionário")
+        print("4 - Listar Funcionários")
+        print("5 - Realizar Pagamento")
+        print("0 - Retornar")
+        return int(input("Escolha a opção: "))
 
-class TelaRecursosHumanos():
+    def pega_dados_funcionario(self):
+        print("\n--- DADOS DO FUNCIONÁRIO ---")
+        nome = input("Nome: ")
+        cpf = input("CPF: ")
+        idade = int(input("Idade: "))
+        telefone = input("Telefone: ")
+        email = input("Email: ")
+        tipo_cargo = input("Cargo: ")
+        return {
+            "nome": nome,
+            "cpf": cpf,
+            "idade": idade,
+            "telefone": telefone,
+            "email": email,
+            "tipo_cargo": tipo_cargo
+        }
+
+    def seleciona_funcionario(self):
+        return input("Digite o CPF do funcionário: ")
+
+    def pega_metodo_pagamento(self):
+        return input("Método de pagamento: ")
+
+    def mostra_lista(self, lista):
+        print("\n--- FUNCIONÁRIOS CADASTRADOS ---")
+        for item in lista:
+            print(item)
+
+    def mostra_mensagem(self, msg):
+        print(msg)
