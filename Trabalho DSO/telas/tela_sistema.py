22a186e247d6c0593885354697585ccdf7e0915c
gabriela-ssj <gabrelajesus755@gmail.com>
2025-10-05 18:22:05 -0300

    implemenação controle e tela main

--- Trabalho DSO/telas/tela_sistema.py
+++ Trabalho DSO/telas/tela_sistema.py
@@ -1,2 +1,9 @@
+class TelaSistema:
+    def tela_opcoes(self):
+        print("\n--- MENU PRINCIPAL DO SISTEMA DE HOTÉIS ---")
+        print("1 - Gerenciar Hotéis")
+        print("0 - Sair")
+        return int(input("Escolha a opção: "))
 
-class TelaSistema():
+    def mostra_mensagem(self, msg):
+        print(msg)
