120e0f3684dbf2d17baed461fe85c1e44a1a6d8f
gabriela-ssj <gabrelajesus755@gmail.com>
2025-10-10 23:27:10 -0300

    ajuste em pet para adicionar opçao alterar, e validador de cpf

--- Trabalho DSO/telas/tela_pet.py
+++ Trabalho DSO/telas/tela_pet.py
@@ -1,34 +1,29 @@
-class TelaPet:
+from telas.tela_abstrata import TelaAbstrata
+
+class TelaPet(TelaAbstrata):
     def tela_opcoes(self):
         print("\n-------- MENU PETS ----------")
         print("1 - Cadastrar Pet")
         print("2 - Listar Pets")
         print("3 - Remover Pet")
+        print("4 - Alterar Pet")
         print("0 - Retornar")
-        try:
-            return int(input("Escolha a opção: "))
-        except ValueError:
-            self.mostra_mensagem("Valor inválido!")
-            return -1
+        return self.le_num_inteiro("Escolha a opção: ", [0, 1, 2, 3, 4])
 
     def pega_dados_pet(self):
-        print("\n--- CADASTRO DE PET ---")
-        cpf_hospede = input("CPF do hóspede: ")
-        nome_pet = input("Nome do pet: ")
-        especie = input("Espécie: ")
-        try:
-            quant_pet = int(input("Quantidade: "))
-        except ValueError:
-            self.mostra_mensagem("Quantidade inválida!")
-            return None, None
+        print("\n--- DADOS DO PET ---")
+        cpf_hospede = self.valida_cpf()
+        nome_pet = self.le_string("Nome do pet: ")
+        especie = self.le_string("Espécie: ")
+        quant_pet = self.le_num_inteiro("Quantidade: ")
         dados_pet = {"nome_pet": nome_pet, "especie": especie, "quant_pet": quant_pet}
         return cpf_hospede, dados_pet
 
-    def seleciona_pet_para_remover(self):
-        print("\n--- REMOVER PET ---")
-        cpf_hospede = input("CPF do hóspede: ")
-        nome_pet = input("Nome do pet a remover: ")
+    def seleciona_pet(self):
+        cpf_hospede = self.valida_cpf("CPF do hóspede: ")
+        nome_pet = self.le_string("Nome do pet: ")
         return cpf_hospede, nome_pet
+
 
     def mostra_lista(self, lista):
         print("\n--- LISTA DE PETS ---")
