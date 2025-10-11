6ec3c4ecdde6fb61c3888db1ca911e29cbe237c9
Camila Martello <mvrtello.cvmila@gmail.com>
2025-10-10 21:26:45 -0300

    Retifiquei TelaServicoDeQuarto

--- Trabalho DSO/telas/tela_servico_de_quarto.py
+++ Trabalho DSO/telas/tela_servico_de_quarto.py
@@ -1,11 +1,10 @@
-class Tela_Servico_de_Quarto:
+class TelaServicoDeQuarto:
     def tela_opcoes(self):
         print("\n-------- MENU SERVIÇO DE QUARTO ----------")
         print("1 - Adicionar Serviço de Quarto")
         print("2 - Listar Serviços de Quarto")
         print("0 - Retornar")
-        try
+        try:
             return int(input("Escolha a opção: "))
         except ValueError:
             print("⚠️ Valor inválido!")
-   
