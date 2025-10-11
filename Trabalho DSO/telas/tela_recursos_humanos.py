0fe9121957229d9607e41d5e209d39f48cde7bdf
gabriela-ssj <gabrelajesus755@gmail.com>
2025-10-11 17:45:43 -0300

    impementei a tela abstarta em tela de rh

--- Trabalho DSO/telas/tela_recursos_humanos.py
+++ Trabalho DSO/telas/tela_recursos_humanos.py
@@ -1,4 +1,6 @@
-class TelaRh:
+from telas.tela_abstrata import TelaAbstrata
+
+class TelaRh(TelaAbstrata):
     def tela_opcoes(self):
         print("\n-------- MENU RH ----------")
         print("1 - Incluir Funcionário")
@@ -7,16 +9,16 @@ class TelaRh:
         print("4 - Listar Funcionários")
         print("5 - Realizar Pagamento")
         print("0 - Retornar")
-        return int(input("Escolha a opção: "))
+        return self.le_num_inteiro("Escolha a opção: ", [0, 1, 2, 3, 4, 5])
 
     def pega_dados_funcionario(self):
         print("\n--- DADOS DO FUNCIONÁRIO ---")
-        nome = input("Nome: ")
-        cpf = input("CPF: ")
-        idade = int(input("Idade: "))
-        telefone = input("Telefone: ")
-        email = input("Email: ")
-        tipo_cargo = input("Cargo: ")
+        nome = self.le_string("Nome: ")
+        cpf = self.le_string("CPF: ")
+        idade = self.le_num_inteiro("Idade: ")
+        telefone = self.le_string("Telefone: ")
+        email = self.le_string("Email: ")
+        tipo_cargo = self.le_string("Cargo: ")
         return {
             "nome": nome,
             "cpf": cpf,
@@ -27,10 +29,10 @@ class TelaRh:
         }
 
     def seleciona_funcionario(self):
-        return input("Digite o CPF do funcionário: ")
+        return self.le_string("Digite o CPF do funcionário: ")
 
     def pega_metodo_pagamento(self):
-        return input("Método de pagamento: ")
+        return self.le_string("Método de pagamento: ")
 
     def mostra_lista(self, lista):
         print("\n--- FUNCIONÁRIOS CADASTRADOS ---")
