b1f9510866da4b97979f5cc72dfa75830fbcc209
Camila Martello <mvrtello.cvmila@gmail.com>
2025-11-16 11:41:27 -0300

    refatorando a classe ControladorCargo

--- Trabalho DSO/entidades/cargo.py
+++ Trabalho DSO/entidades/cargo.py
@@ -1,3 +1,4 @@
+# entidades/cargo.py
 class Cargo:
     _salarios_por_cargo = {
         "gerente": 5000.0,
@@ -9,10 +10,12 @@ class Cargo:
     }
 
     def __init__(self, tipo_cargo: str):
+        # AQUI, o construtor espera APENAS 'tipo_cargo'
         tipo_cargo = tipo_cargo.lower()
         if tipo_cargo not in self._salarios_por_cargo:
             raise ValueError(f"Tipo de cargo inválido: '{tipo_cargo}'. Opções válidas: {', '.join(self._salarios_por_cargo.keys())}")
         self._tipo_cargo = tipo_cargo
+        # O salario_base é DEFINIDO INTERNAMENTE com base no tipo_cargo
         self._salario_base = self._salarios_por_cargo[tipo_cargo]
 
     @property
