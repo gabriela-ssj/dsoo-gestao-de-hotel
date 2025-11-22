b4b01111726d3733823d79bd9c1e62dee522328c
gabriela-ssj <gabrelajesus755@gmail.com>
2025-11-22 19:23:07 -0300

    ajuste em reserva_dao

--- Trabalho DSO/daos/reserva_dao.py
+++ Trabalho DSO/daos/reserva_dao.py
@@ -5,23 +5,33 @@ from entidades.reserva import Reserva
 class ReservaDAO(DAO):
     def __init__(self):
         super().__init__("reservas.pkl")
+
         self.__ultimo_id = self._descobrir_ultimo_id()
 
     def _descobrir_ultimo_id(self):
-        todos = self.get_all()
-        if not todos:
+        todas = list(super().get_all()) 
+
+        if not todas:
             return 0
-        return max(reserva.id for reserva in todos)
+
+        try:
+            return max(reserva.id for reserva in todas if reserva.id is not None)
+        except:
+            return 0
 
     def add(self, reserva: Reserva):
         if reserva is not None and isinstance(reserva, Reserva):
+
+            self.__ultimo_id += 1
+            reserva.id = self.__ultimo_id
+
             super().add(reserva.id, reserva)
 
     def get(self, key: int):
         return super().get(key)
 
     def remove(self, key: int):
-        return super().remove(key)
+        super().remove(key)
 
     def get_all(self):
-        return super().get_all()
+        return list(super().get_all())
