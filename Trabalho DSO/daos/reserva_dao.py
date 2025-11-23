35debbc6b0a98d5414feaf7b69b4d685bd6c9564
gabriela-ssj <gabrelajesus755@gmail.com>
2025-11-22 22:27:12 -0300

    retornei a dao anterior de reserva, pois nao consegui arrumar o id acrescentar id ja existente

--- Trabalho DSO/daos/reserva_dao.py
+++ Trabalho DSO/daos/reserva_dao.py
@@ -5,33 +5,23 @@ from entidades.reserva import Reserva
 class ReservaDAO(DAO):
     def __init__(self):
         super().__init__("reservas.pkl")
-
         self.__ultimo_id = self._descobrir_ultimo_id()
 
     def _descobrir_ultimo_id(self):
-        todas = list(super().get_all()) 
-
-        if not todas:
+        todos = self.get_all()
+        if not todos:
             return 0
-
-        try:
-            return max(reserva.id for reserva in todas if reserva.id is not None)
-        except:
-            return 0
+        return max(reserva.id for reserva in todos)
 
     def add(self, reserva: Reserva):
         if reserva is not None and isinstance(reserva, Reserva):
-
-            self.__ultimo_id += 1
-            reserva.id = self.__ultimo_id
-
             super().add(reserva.id, reserva)
 
     def get(self, key: int):
         return super().get(key)
 
     def remove(self, key: int):
-        super().remove(key)
+        return super().remove(key)
 
     def get_all(self):
-        return list(super().get_all())
+        return super().get_all()
